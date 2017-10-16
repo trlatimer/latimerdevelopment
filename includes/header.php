d9ece77b713a5c028973a69a0cdf0d2c3c614586
Tyler Latimer <trlatimer95@gmail.com>
2017-10-15 20:04:53 -0800

    Add styles to contact.php and portfolio.php
    Add styles to contact.php and portfolio.php, rework portfolio.php to improve functionality, add style links in header

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link type="text/css" rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="portfolioStyles.css">
+        <link rel="stylesheet" href="contactStyles.css">
         <link type="text/javascript" href="/js/app.js">
     </head>
     <body>
--- portfolio.php
+++ portfolio.php
@@ -4,31 +4,43 @@
 ?>
 
 <section class="portfolioPage">
-    <div class="portfolioBox">
-        <h1>Welcome to My Portfolio!</h1>
-        <div class="portolioPageGrid">
-            <figure class="project1">
-                <img src="images/TGK.jpg" alt="The Great Kingdom">
-                <figcaption>
-                    <h4>The Great Kingdom</h4>
-                    <p class="content">The Great Kingdom is my first personal project outside of this website. I created it for a gaming community I am involved in. It involves various features such as users and profiles as well as a Steam API. This site uses node.js with express and several addons.</p>
-                    <p><a href="#" class="btn btn-success" role="button">View</a></p>
+    <div class="portfolioContainer">
+        <h1>My Portfolio</h1>
+        <p>Click images to view live or download!</p>
+        <div class="portfolioPageGrid">
+            <figure class="portfolioItem" item="portfolioPage1">
+                <a href="https://github.com/trlatimer/ud036_StarterCode"><img src="images/movie.jpg" alt="Movie website screenshot"></a>
+                <figcaption class="description">
+                    <h2>Movie Website</h2>
+                    <p>This website lists movies and displays the poster image, trailer, and description. This project was created with python and bootstrap</p>
                 </figcaption>
             </figure>
-            <figure class="project2">
-                <img src="images/PatatapClone.png" alt="PatatapClone">
-                <figcaption>
-                    <h4>Patatap Clone</h4>
-                    <p>The Patatap Clone projet was a project I completed during the Web Developer Bootcamp course taught by Colt Steele on Udemy.com. This project is made using jQuery and creates sound and circles when a button is pushed. Every button creates a different color of circle as well as a different sound.</p>
-                    <p><a href="http://latimerdevelopment.com/patatapclone/circles.html" class="btn btn-success" role="button">View</a></p>
+            <figure class="portfolioItem" item="portfolioPage2">
+                <a href="http://www.latimerdevelopment.com/portfolioProject/"><img src="images/porftolioProject.png" alt="Portfolio project screenshot"></a>
+                <figcaption class="description">
+                    <h2>Portfolio Project</h2>
+                    <p>Based off of a PSD layout, this project is customized to fit my style and displays projects I have completed and am working on.</p>
                 </figcaption>
             </figure>
-            <figure class="project3">
-                <img src="images/Orbit.png" alt="Orbit Project">
-                <figcaption>
-                    <h4>Orbit Animation</h4>
+            <figure class="portfolioItem" item="portfolioPage3">
+                <a href="http://www.latimerdevelopment.com/codePHP/"><img src="images/franklins.png" alt="Franklin's screenshot"></a>
+                <figcaption class="description">
+                    <h2>Franklin's Fine Dining</h2>
+                    <p>A sample website for a small diner. Created with PHP, HTML, and CSS. This project was created during the Practical PHP: Master the Basics and Code Dynamic Websites course taught by Brad Hussey on Udemy.com</p>
+                </figcaption>
+            </figure>
+            <figure class="portfolioItem" item="portfolioPage4">
+                <a href="http://latimerdevelopment.com/orbitproject/orbit.html"><img src="images/Orbit.png" alt="Orbit screenshot"></a>
+                <figcaption class="description">
+                    <h2>Orbit Animation</h2>
                     <p>The Orbit animation is a project I completed during the Ultimate Web Designer &amp; Developer Course taught by Brad Hussey on Udemy.com. This project is made only with HTML and CSS. The animation shows the Earth rotating around the sun with the moon rotating around the Earth.</p>
-                    <p><a href="http://latimerdevelopment.com/orbitproject/orbit.html" class="btn btn-success" role="button">View</a></p>
+                </figcaption>
+            </figure>
+            <figure class="portfolioItem" item="portfolioPage5">
+                <a href="http://latimerdevelopment.com/patatapclone/circles.html"><img src="images/PatatapClone.png" alt="Patatap Clone screenshot"></a>
+                <figcaption class="description">
+                    <h2>Patatap Clone</h2>
+                    <p>The Patatap Clone projet was a project I completed during the Web Developer Bootcamp course taught by Colt Steele on Udemy.com. This project is made using jQuery and creates sound and circles when a button is pushed. Every button creates a different color of circle as well as a different sound.</p>
                 </figcaption>
             </figure>
         </div>
